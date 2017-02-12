dff6f955591b08250be2aa66c8f75fde692330d1
Alex Mackenzie <alexmack929@googlemail.com>
2017-02-12 02:00:50 +0000

    Fix whitespace in Texture.java

--- src/net/alexmack/boothy/textures/Texture.java
+++ src/net/alexmack/boothy/textures/Texture.java
@@ -16,69 +16,69 @@ public class Texture {
 			1, 1,
 			0, 1
 	};
-	
+
 	private TextureBinding binding;
-	
+
 	private int width, height;
 	private float[] coordinates;
-	
+
 	public Texture(BufferedImage image) {
 		this(new TextureBinding(image));
 	}
-	
+
 	public Texture(TextureBinding binding) {
 		this(binding, binding.getWidth(), binding.getHeight(), DEFAULT_COORDINATES);
 	}
-	
+
 	public Texture(TextureBinding binding, int width, int height, float[] coordinates) {
 		this.binding = binding;
 		this.width = width;
 		this.height = height;
 		this.coordinates = coordinates;
 	}
-	
+
 	public void draw(int x, int y) {
 		draw(x, y, width, height);
 	}
-	
+
 	public void draw(int x, int y, int w, int h) {
 		// Don't draw using a binding that hasn't been loaded.
 		if (!isReady())
 			return;
-		
+
 		binding.bind();
-		
+
 		GL11.glBegin(GL11.GL_QUADS);
 		{
 			GL11.glTexCoord2f(coordinates[0], coordinates[1]);
-            GL11.glVertex2f(x, y);
-            
-            GL11.glTexCoord2f(coordinates[2], coordinates[3]);
-            GL11.glVertex2f(x + w, y);
-            
-            GL11.glTexCoord2f(coordinates[4], coordinates[5]);
-            GL11.glVertex2f(x + w, y + h);
-            
-            GL11.glTexCoord2f(coordinates[6], coordinates[7]);
-            GL11.glVertex2f(x, y + h);
+			GL11.glVertex2f(x, y);
+
+			GL11.glTexCoord2f(coordinates[2], coordinates[3]);
+			GL11.glVertex2f(x + w, y);
+
+			GL11.glTexCoord2f(coordinates[4], coordinates[5]);
+			GL11.glVertex2f(x + w, y + h);
+
+			GL11.glTexCoord2f(coordinates[6], coordinates[7]);
+			GL11.glVertex2f(x, y + h);
 		}
 		GL11.glEnd();
 	}
-	
+
 	public int getWidth() {
 		return width;
 	}
-	
+
 	public int getHeight() {
 		return height;
 	}
-	
+
 	public boolean isReady() {
 		return binding.isReady();
 	}
-	
+
 	public TextureBinding getBinding() {
 		return binding;
 	}
-	
+
 }
