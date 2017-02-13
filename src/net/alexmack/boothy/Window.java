86cd0178ec2e09b32fdf71d629e09747f275db1c
Alex Mackenzie <alexmack929@googlemail.com>
2017-02-13 23:12:55 +0000

    Change default scaling

--- src/net/alexmack/boothy/Window.java
+++ src/net/alexmack/boothy/Window.java
@@ -155,9 +155,9 @@ public class Window {
 		// Enable 2D textures.
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		
-		// Default scaling.
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
-		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		// Default nearest neighbour scaling.
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
 	}
 	
 	/**
