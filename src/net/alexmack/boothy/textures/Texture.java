7a7c3ed6f935df0a4d65b4a5ad0b6440031762f5
Alex Mackenzie <alexmack929@googlemail.com>
2017-02-13 23:48:47 +0000

    Fixed coordinates and added translations

--- src/net/alexmack/boothy/textures/Texture.java
+++ src/net/alexmack/boothy/textures/Texture.java
@@ -8,13 +8,37 @@ import java.awt.image.BufferedImage;
 import org.lwjgl.opengl.GL11;
 
 public class Texture {
-
+	
+	public static final int[] TRANSLATION_NORMAL = new int[] {
+			// Top left.
+			0, 2,
+			// Top right.
+			1, 2,
+			// Bottom right.
+			1, 3,
+			// Bottom left.
+			0, 3
+	};
+	
+	public static final int[] TRANSLATION_FLIP_H = new int[] {
+			1, 3,
+			0, 3,
+			0, 2,
+			1, 2
+	};
+	
+	public static final int[] TRANSLATION_FLIP_V = new int[] {
+			1, 2,
+			0, 2,
+			0, 3,
+			1, 3
+	};
+	
 	// The default texture coordinates, encompassing the entire image.
 	private static final float[] DEFAULT_COORDINATES = new float[] {
-			// TODO: Redo coordinate system that's been carried over from Boothium, it's wasteful.
-			0, 0,
-			1, 0,
-			1, 1,
+			// X origin and end.
+			0, 1,
+			// Y origin and end.
 			0, 1
 	};
 
@@ -41,8 +65,16 @@ public class Texture {
 	public void draw(int x, int y) {
 		draw(x, y, width, height);
 	}
-
+	
+	public void draw(int x, int y, int[] t) {
+		draw(x, y, width, height, t);
+	}
+	
 	public void draw(int x, int y, int w, int h) {
+		draw(x, y, w, h, TRANSLATION_NORMAL);
+	}
+
+	public void draw(int x, int y, int w, int h, int[] t) {
 		// Don't draw using a binding that hasn't been loaded.
 		if (!isReady())
 			return;
@@ -51,20 +83,20 @@ public class Texture {
 
 		GL11.glBegin(GL11.GL_QUADS);
 		{
-			// Top left.
-			GL11.glTexCoord2f(coordinates[0], coordinates[1]);
+			// First corner, top left when using standard translation.
+			GL11.glTexCoord2f(coordinates[t[0]], coordinates[t[1]]);
 			GL11.glVertex2f(x, y);
 
-			// Top right.
-			GL11.glTexCoord2f(coordinates[2], coordinates[3]);
+			// Second corner, top right when using standard translation.
+			GL11.glTexCoord2f(coordinates[t[2]], coordinates[t[3]]);
 			GL11.glVertex2f(x + w, y);
 
-			// Bottom right.
-			GL11.glTexCoord2f(coordinates[4], coordinates[5]);
+			// Third corner, bottom right when using standard translation.
+			GL11.glTexCoord2f(coordinates[t[4]], coordinates[t[5]]);
 			GL11.glVertex2f(x + w, y + h);
 
-			// Bottom left.
-			GL11.glTexCoord2f(coordinates[6], coordinates[7]);
+			// Fourth corner, bottom left when using standard translation.
+			GL11.glTexCoord2f(coordinates[t[6]], coordinates[t[7]]);
 			GL11.glVertex2f(x, y + h);
 		}
 		GL11.glEnd();
@@ -85,9 +117,9 @@ public class Texture {
 		
 		// Get coordinates of the original image.
 		float originX = coordinates[0];
-		float originY = coordinates[1];
-		float endX = coordinates[2];
-		float endY = coordinates[5];
+		float originY = coordinates[2];
+		float endX = coordinates[1];
+		float endY = coordinates[3];
 		
 		// Width and height in texture terms.
 		float texWidth = endX - originX;
@@ -109,10 +141,8 @@ public class Texture {
 		
 		// Put these into an array.
 		float[] cut = new float[] {
-				cutOriginX, cutOriginY,
-				cutEndX, cutOriginY,
-				cutEndX, cutEndY,
-				cutOriginX, cutEndY
+				cutOriginX, cutEndX,
+				cutOriginY, cutEndY
 		};
 		
 		return new Texture(binding, w, h, cut);
