0bf5fd3d74f6a6833b02e5ceadb3a1d6b1a4d293
Alex Mackenzie <alexmack929@googlemail.com>
2018-01-11 20:27:30 +0000

    Add alpha functions to TextureColor

--- src/net/alexmack/boothy/textures/TextureColor.java
+++ src/net/alexmack/boothy/textures/TextureColor.java
@@ -11,16 +11,28 @@ public class TextureColor {
 		push(rgb >>> 16, (rgb >>> 8) & 0xFF, rgb & 0xFF);
 	}
 	
+	public static void pushA(int rgb, int alpha) {
+		pushA(rgb >>> 16, (rgb >>> 8) & 0xFF, rgb & 0xFF, alpha);
+	}
+	
 	public static void push(int r, int g, int b) {
 		push((float) r / 255F, (float) g / 255F, (float) b / 255F);
+	}
+	
+	public static void pushA(int r, int g, int b, int alpha) {
+		pushA((float) r / 255F, (float) g / 255F, (float) b / 255F, (float) alpha / 255F);
 	}
 	
 	public static void push(float r, float g, float b) {
 		GL11.glColor3f(r, g, b);
 	}
 	
+	public static void pushA(float r, float g, float b, float alpha) {
+		GL11.glColor4f(r, g, b, alpha);
+	}
+	
 	public static void pop() {
-		GL11.glColor3f(1, 1, 1);
+		GL11.glColor4f(1, 1, 1, 1);
 	}
 	
 }
